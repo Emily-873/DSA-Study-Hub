9abd0c2426a5b9a083dbd89bad6586ee305e1982
toxicbishop <pranavarun19@gmail.com>
2026-06-16 10:15:54 +0530

    fix(deps): commit pnpm hook and update lockfile to resolve js-yaml DoS

---
+++ server/.pnpmfile.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  hooks: {
+    readPackage(pkg) {
+      if (pkg.dependencies && pkg.dependencies['js-yaml']) {
+        // console.log("Hooking dependencies for", pkg.name);
+        pkg.dependencies['js-yaml'] = '4.2.0';
+      }
+      if (pkg.devDependencies && pkg.devDependencies['js-yaml']) {
+        pkg.devDependencies['js-yaml'] = '4.2.0';
+      }
+      return pkg;
+    }
+  }
+};
