d3b2dd463b7aa0d0bbe4503134ee28d9d9429d24
toxicbishop <pranavarun19@gmail.com>
2026-07-24 22:53:43 +0530

    fix: resolve stubborn js-yaml and sharp versions via pnpm hooks and workspace overrides

--- server/.pnpmfile.cjs
+++ server/.pnpmfile.cjs
@@ -3,10 +3,10 @@ module.exports = {
     readPackage(pkg) {
       if (pkg.dependencies && pkg.dependencies['js-yaml']) {
         // console.log("Hooking dependencies for", pkg.name);
-        pkg.dependencies['js-yaml'] = '4.2.0';
+        pkg.dependencies['js-yaml'] = '^4.3.0';
       }
       if (pkg.devDependencies && pkg.devDependencies['js-yaml']) {
-        pkg.devDependencies['js-yaml'] = '4.2.0';
+        pkg.devDependencies['js-yaml'] = '^4.3.0';
       }
       return pkg;
     }
